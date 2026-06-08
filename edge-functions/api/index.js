7fff54470a88391213033f2814e688da11acaf17
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-06-08 22:20:55 +0800

    Update index.js

--- edge-functions/api/index.js
+++ edge-functions/api/index.js
@@ -4,5 +4,5 @@ export async function onRequest(context) {
     const url = new URL(request.url);
 
     // 直接重定向到 dnsindex.html，不再拦截密码
-    return Response.redirect(url.origin + '/dnsindex.html', 302);
+    return Response.redirect(url.origin + '/index.html', 302);
 }
