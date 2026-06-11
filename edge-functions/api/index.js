fab5d04bc61200e447bd99eed7069a626373c8d3
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-06-11 13:45:44 +0800

    Delete edge-functions/api/index.js

--- edge-functions/api/index.js
+++
@@ -1,8 +0,0 @@
-// edge-functions/index.js
-export async function onRequest(context) {
-    const { request, env } = context;
-    const url = new URL(request.url);
-
-    // 直接重定向到 dnsindex.html，不再拦截密码
-    return Response.redirect(url.origin + '/index.html', 302);
-}
